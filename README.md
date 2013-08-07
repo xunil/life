bef248d723d368d05f8aa06c3d3b45e454f2856b
Robert Liesenfeld <xunil@squareup.com>
2013-08-06 19:07:13 -0700

    OOP instead of procedural, fixed nearly-empty board converging on fully-populated board, TODO list

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 I'm an Ops guy, not a software engineer, so I've never written Conway's Game of Life for a CS class or anything.  Thought it might be a fun project, so here we are.
+
+# TODO
+- Implement wraparound liveness checking - make the game board a sphere, not a 2D grid
+- Figure out better seeding of the board
+- Implement seeding with arbitrary predefined shapes on an otherwise empty board (e.g. puffer glider factory)
+- Graphical display instead of text?
+- Use escape codes or SIGWINCH or something to determine terminal size
+- Quit via keypress
+- Step generation via keypress
+- Status bar showing generation number, timing stats
--- life.rb
+++ life.rb
@@ -1,89 +1,106 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
-ROWS = 40
-COLS = 100
-GRID_SIZE = ROWS * COLS
-
-def each_cell
-  ROWS.times do |row|
-    COLS.times do |col|
-      i = (COLS*row)+col
-      yield i, row, col
-    end
+class Life
+  attr_reader :rows, :cols, :grid_size
+  def initialize(rows, cols, seed_modulo=rand(100))
+    @rows = rows
+    @cols = cols
+    @seed_modulo = seed_modulo
+    @grid_size = rows * cols
+    @cells = Array.new(@grid_size)
+    (@grid_size).times {|i| @cells[i] = (rand(@grid_size) % (@seed_modulo) == 0)}
   end
-end
 
-def display_board(cells)
-  print "\e[2J"
-  $stdout.flush
-  each_cell do |i,row,col|
-    print "%s" % (cells[i] ? '█' : ' ')
-    puts if i % COLS == 0
-  end
-end
-
-# assuming 10x10 grid:
-# [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
-# [  ][  ][  ][AL][ A][AR][  ][  ][  ][  ]
-# [  ][  ][  ][ L][ 0][ R][  ][  ][  ][  ] <- 0 == row 2, col 4
-# [  ][  ][  ][BL][ B][BR][  ][  ][  ][  ]
-# [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
-# [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
-# [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
-# [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
-# [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
-# [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
-# So:
-# L and R are easy; col-1 and col+1 respectively
-# A and B are easy; row-1 and row+1
-# AL == row-1, col-1
-# AR == row-1, col+1
-# BL == row+1, col-1
-# BR == row+1, col+1
-#
-# if row == 0, don't check above
-# if col == 0, don't check left
-# if row == ROWS, don't check below
-# if col == COLS, don't check right
-# Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-# Any live cell with two or three live neighbours lives on to the next generation.
-# Any live cell with more than three live neighbours dies, as if by overcrowding.
-# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-def living?(cells, row, col)
-  return cells[COLS*row+col]
-end
-def generation(cells)
-  each_cell do |i,row,col|
-    living_neighbors = 0
-    if row > 0
-      if col > 0
-        living_neighbors += 1 if living?(cells, row-1, col-1)
-        living_neighbors += 1 if living?(cells, row, col-1)
-        if row < ROWS
-          living_neighbors += 1 if living?(cells, row+1, col-1)
-        end
-      end
-      living_neighbors += 1 if living?(cells, row-1, col)
-      if col < COLS
-        living_neighbors += 1 if living?(cells, row-1, col+1)
-        living_neighbors += 1 if living?(cells, row, col+1)
-        if row < ROWS
-          living_neighbors += 1 if living?(cells, row+1, col+1)
-        end
+  def each_cell
+    @rows.times do |row|
+      @cols.times do |col|
+        i = (@cols*row)+col
+        yield i, row, col
       end
     end
-    cells[i] = false
-    cells[i] = true if (living_neighbors == 2 or living_neighbors == 3)
+  end
+
+  def clear_screen!
+    print "\e[2J" # Hope you like VT100 escape codes
+    $stdout.flush
+  end
+
+  def display_board
+    clear_screen!
+    each_cell do |i,row,col|
+      print "%s" % (@cells[i] ? '█' : ' ')
+      puts if i % @cols == 0
+    end
+  end
+
+  def living?(row, col)
+    return @cells[@cols*row+col]
+  end
+
+  # assuming 10x10 grid:
+  # [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
+  # [  ][  ][  ][AL][ A][AR][  ][  ][  ][  ]
+  # [  ][  ][  ][ L][ 0][ R][  ][  ][  ][  ] <- 0 == row 2, col 4
+  # [  ][  ][  ][BL][ B][BR][  ][  ][  ][  ]
+  # [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
+  # [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
+  # [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
+  # [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
+  # [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
+  # [  ][  ][  ][  ][  ][  ][  ][  ][  ][  ]
+  # So:
+  # L and R are easy; col-1 and col+1 respectively
+  # A and B are easy; row-1 and row+1
+  # AL == row-1, col-1
+  # AR == row-1, col+1
+  # BL == row+1, col-1
+  # BR == row+1, col+1
+  #
+  # if row == 0, don't check above
+  # if col == 0, don't check left
+  # if row == ROWS, don't check below
+  # if col == COLS, don't check right
+  # Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+  # Any live cell with two or three live neighbours lives on to the next generation.
+  # Any live cell with more than three live neighbours dies, as if by overcrowding.
+  # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+  def generation
+    new_grid = Array.new(@grid_size)
+    each_cell do |i,row,col|
+      living_neighbors = 0
+      if row > 0
+        if col > 0
+          living_neighbors += 1 if living?(row-1, col-1)
+          living_neighbors += 1 if living?(row, col-1)
+          if row < @rows
+            living_neighbors += 1 if living?(row+1, col-1)
+          end
+        end
+        living_neighbors += 1 if living?(row-1, col)
+        if col < @cols
+          living_neighbors += 1 if living?(row-1, col+1)
+          living_neighbors += 1 if living?(row, col+1)
+          if row < @rows
+            living_neighbors += 1 if living?(row+1, col+1)
+          end
+        end
+      end
+      new_grid[i] = ((living_neighbors == 2 or living_neighbors == 3) ? true : false)
+    end
+    @cells = new_grid
+  end
+  def alive?
+    @cells.include?(true)
   end
 end
 
-cells = Array.new(GRID_SIZE)
-(GRID_SIZE).times {|i| cells[i] = (rand(GRID_SIZE) % (ARGV.first.to_i) == 0)}
+l = Life.new(40, 100, 103)
 
-GRID_SIZE.times do
-  display_board(cells)
-  generation(cells)
-  sleep(0.5)
+100.times do
+  l.display_board
+  l.generation
+  sleep(0.1)
+  break if not l.alive?
 end
 
